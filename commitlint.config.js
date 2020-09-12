ac0f91df27adf08d4d8cae0ca933783d6c6c18af
mrbandler <mrbandler@fivefingergames.com>
2020-09-12 23:06:12 +0200

    chore(upgrade): Upgraded project and dependencies

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 module.exports = {
     extends: ["@commitlint/config-conventional"],
-    parserPreset: "conventional-changelog-atom",
-    formatter: "@commitlint/format",
     rules: {
         "subject-case": [2, "always", "sentence-case"],
         "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "test", "revert", "chore", "merge", "release", "ci", "wiki"]],
